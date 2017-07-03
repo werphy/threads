6ca1b1e1cc67dde213c372f071aa60416d14cbf2
wangbin <364957048@qq.com>
2017-07-03 21:44:07 +0800

    firstThread

--- src/FirstThread.java
+++ src/FirstThread.java
@@ -1,4 +1,17 @@
 
-public class FirstThread extends Thread {
-
+public class FirstThread extends Thread {	
+	public static void main(String[] args) {
+		
+		Thread first =new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(true){			
+		        System.out.println("这是线程:"+Thread.currentThread().getName());
+				}
+			}
+		},"线程1");
+		first.start();
+	}
+	
+	
 }
